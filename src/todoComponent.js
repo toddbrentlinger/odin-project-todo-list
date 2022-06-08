01c6d8c76e67611a8a262b58430871135b8c38f5
toddbrentlinger <toddbrentlinger@gmail.com>
2022-06-07 17:53:04 -0700

    Start ToDo dropdown element to display more details

--- src/todoComponent.js
+++ src/todoComponent.js
@@ -3,28 +3,115 @@ import { createElement } from "./utilities.js";
 import './todoComponent.scss';
 
 export default function ToDoComponent(todo) {
+    let _todoElement = null;
+    let _detailsDropdownContainerElement = null;
+    let _bIsExpanded = false;
+
+    const _handleExpandBtnClick = () => {
+        if (_bIsExpanded) {
+            _closeDetailsDropdown();
+        } else {
+            _openDetailsDropdown();
+        }
+    };
+
+    const _createToDoDropdownElement = () => {
+        _detailsDropdownContainerElement = createElement('div', {'class': 'todo-item-details-container'});
+
+        const detailsDropdownElement = _detailsDropdownContainerElement.appendChild(
+            createElement('div', {'class': 'todo-item-details'})
+        );
+
+        // Description
+        detailsDropdownElement.appendChild(
+            createElement('p', {'class': 'todo-description todo-details-item'}, todo.getDescription())
+        );
+
+        // Priority
+        const priorityLevel = todo.getPriorityLevel();
+        detailsDropdownElement.appendChild(
+            createElement('div', {'class': 'todo-priority-container todo-details-item'}, 
+                createElement('div', {}, 'Priority: '),
+                createElement('div', {}, `(${priorityLevel.getValue()}) ${priorityLevel.getColor()}`)
+            )
+        );
+
+        // Repeat
+        const repeatType = todo.getRepeatType();
+        detailsDropdownElement.appendChild(
+            createElement('div', {'class': 'todo-repeat-container todo-details-item'}, 
+                createElement('div', {}, 'Repeat: '),
+                createElement('div', {}, repeatType.getName())
+            )
+        );
+
+        // Project
+        const project = todo.getProject();
+        detailsDropdownElement.appendChild(
+            createElement('div', {'class': 'todo-project-container todo-details-item'}, 
+                createElement('div', {}, 'Project: '),
+                createElement('div', {}, project.getName())
+            )
+        );
+
+        // Btn Container
+        const btnContainer = detailsDropdownElement.appendChild(
+            createElement('div', {'class': 'todo-btn-container'})
+        );
+
+        // Btn - Delete
+        btnContainer.appendChild(
+            createElement('button', {'class': 'todo-btn-delete'}, 'Delete')
+        );
+
+        // Btn - Edit
+        btnContainer.appendChild(
+            createElement('button', {'class': 'todo-btn-edit'}, 'Edit')
+        );
+
+        return _detailsDropdownContainerElement;
+    };
+
+    const _openDetailsDropdown = () => {
+        _todoElement.appendChild(_createToDoDropdownElement());
+        _bIsExpanded = true;
+    };
+
+    const _closeDetailsDropdown = () => {
+        if (!_detailsDropdownContainerElement)
+            return;
+
+            _detailsDropdownContainerElement.remove();
+        _bIsExpanded = false;
+    };
+
     return {
         getToDo: () => todo,
         render: () => {
-            const todoElement = createElement('div', {'class': 'todo-item'});
+            _todoElement = createElement('div', {'class': 'todo-item'});
 
             // Apply styling depending on ToDo priority level
-            todoElement.style.borderColor = todo.getPriorityLevel().getColor();
+            _todoElement.style.borderColor = todo.getPriorityLevel().getColor();
+
+            // ToDo Header
+            const headerElement = _todoElement.appendChild(
+                createElement('div', {'class': 'todo-item-header'})
+            );
 
             // Checkbox
-            todoElement.appendChild(
+            headerElement.appendChild(
                 createElement('div', {'class': 'todo-checkbox-container'}, 
                     createElement('div', {'class': 'todo-checkbox'})
                 )
             );
 
-            const title = todoElement.appendChild(
+            // Title
+            const title = headerElement.appendChild(
                 createElement('h2', {'class': 'todo-title'}, todo.getTitle())
             );
-            // const description = todoElement.appendChild(
-            //     createElement('p', {'class': 'todo-description'}, todo.getDescription())
-            // );
-            const datetime = todoElement.appendChild(
+
+            // Due Date
+            const datetime = headerElement.appendChild(
                 createElement(
                     'time', 
                     {'class': 'todo-datetime', 'datetime': todo.getDueDateDatetimeAttribute()}, 
@@ -32,7 +119,18 @@ export default function ToDoComponent(todo) {
                 )
             );
 
-            return todoElement;
+            // Expand Button
+            const expandBtn = createElement('button', {'class': 'todo-btn-expand close'}, '+');
+            expandBtn.addEventListener('click', _handleExpandBtnClick, false);
+            headerElement.appendChild(expandBtn);
+
+            if (_bIsExpanded) {
+                _todoElement.appendChild(
+                    _createToDoDropdownElement()
+                );
+            }
+
+            return _todoElement;
         },
     };
 }
